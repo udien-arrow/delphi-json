fe00f4e490b04c8c5a9e1ed48365f0452348859c
udien.arrow <43328624+udien-arrow@users.noreply.github.com>
2024-03-22 08:13:23 +0700

    Update koneksi.php

--- api-json/koneksi.php
+++ api-json/koneksi.php
@@ -4,10 +4,6 @@ $user = 'root';
 $pass = '';
 $database = 'tes_jason';
 
-// $connect_db = mysqli_connect($host, $user, $pass,$database) or die("Koneksi Gagal");
-// //mysqli_select_db($database, $connect_db) or die("Database Tidak Ada");
-
-
 $mysqli = new mysqli($host, $user, $pass, $database);
 
 ?>
